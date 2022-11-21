b64091f93cc5932407dfbe445f9ef176f55fe976
Ale24k <89529810+Ale24k@users.noreply.github.com>
2022-11-21 18:56:20 -0500

    Update nose.py

--- nose.py
+++ nose.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import gdown
+import os
+
 
 st.title('Fallecidos por COVID19')
 
-#Criterio virolÃ³gico = Criterio virológico
+st.title('Fallecidos por COVID19')
+
+
 # id = 1dSRlbtutz10Lgb4wiYPcWaK3w5QMUH8O
 @st.experimental_memo
 def download_data():
@@ -21,12 +25,34 @@ st.dataframe(data.head(20))
 #df = df.drop(columns = ["FECHA_CORTE","FECHA_FALLECIMIENTO","EDAD_DECLARADA","SEXO", "CLASIFICACION_DEF", "DEPARTAMENTO", "PROVINCIA", "DISTRITO", "UBIGEO", "UUID"])
 
 
+#edad = np.sort(df['EDAD_DECLARADA'].dropna().unique())          
+#sexo = np.sort(df['SEXO'].dropna().unique())
 
-option = st.selectbox(
-    ' Lista de fallecidos según el criterio ', CLASIFICACION_DEF))
+edad= data['EDAD_DECLARADA']
+sexo= data['SEXO']
+departamento= data['DEPARTAMENTO'].unique()
+
+#edad= df['EDAD_DECLARADA'].unique().tolist()
+#edad = np.sort(df['EDAD_DECLARADA'].dropna().unique())
+
+#crear un slider de edad
+edad_selector = st.slider('Edad del fallecido: ',
+                         min_value = min(edad),
+                         max_value = max(edad),
+                         value = (min(edad), max(edad)))
+
+
+#sexo_selector = st.multiselect('SEXO:', sexo, default = ciudad)
+st.write(sexo)
+st.write(departamento)
+opcion_departamento = st.selectbox('Selecciona un departamento:', departamento)
+
+number = st.number_input('Ingrese un numero')
+st.write('El numero es ', number)
+
+
+option = st.selectbox(' Lista de fallecidos según el criterio ' , clasificacion_def)
+
 if option == 'Criterio virolÃƒÂ³gico':
     op1 = df["CLASIFICACION_DEF"].value_counts()
     op2 = df["CLASIFICACION_DEF"].value_counts()
-
-    
-    
