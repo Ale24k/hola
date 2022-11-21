45406cb1a1c8a98531c212ef351d363adcebac6c
Ale24k <89529810+Ale24k@users.noreply.github.com>
2022-11-21 18:26:08 -0500

    Update nose.py

--- nose.py
+++ nose.py
@@ -4,11 +4,6 @@ import numpy as np
 import gdown
 
 
-option = st.selectbox(
-    ' Lista de fallecidos según el criterio ',
-    ('Criterio virológico', 'Criterio serológico', 'Criterio radiológico', 'Criterio nexo epidemiológico', 
-     'Criterio investigación epidemiológica', 'Criterio clínico', 'Criterio SINADEF'))
-
 # id = 1dSRlbtutz10Lgb4wiYPcWaK3w5QMUH8O
 @st.experimental_memo
 def download_data():
@@ -22,3 +17,10 @@ download_data()
 data = pd.read_csv('data.csv', sep = ';', parse_dates= ['FECHA_CORTE', 'FECHA_FALLECIMIENTO'])
 st.dataframe(data.head(20))
 #df = df.drop(columns = ["FECHA_CORTE","FECHA_FALLECIMIENTO","EDAD_DECLARADA","SEXO", "CLASIFICACION_DEF", "DEPARTAMENTO", "PROVINCIA", "DISTRITO", "UBIGEO", "UUID"])
+
+
+
+option = st.selectbox(
+    ' Lista de fallecidos según el criterio ',
+    ('Criterio virológico', 'Criterio serológico', 'Criterio radiológico', 'Criterio nexo epidemiológico', 
+     'Criterio investigación epidemiológica', 'Criterio clínico', 'Criterio SINADEF'))
