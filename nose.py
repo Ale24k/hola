2335092f47898388a0829602efcbac883180e3db
Ale24k <89529810+Ale24k@users.noreply.github.com>
2022-11-21 19:18:28 -0500

    Update nose.py

--- nose.py
+++ nose.py
@@ -25,30 +25,6 @@ st.dataframe(data.head(20))
 #df = df.drop(columns = ["FECHA_CORTE","FECHA_FALLECIMIENTO","EDAD_DECLARADA","SEXO", "CLASIFICACION_DEF", "DEPARTAMENTO", "PROVINCIA", "DISTRITO", "UBIGEO", "UUID"])
 
 
-#edad = np.sort(df['EDAD_DECLARADA'].dropna().unique())          
-#sexo = np.sort(df['SEXO'].dropna().unique())
-
-edad= data['EDAD_DECLARADA']
-sexo= data['SEXO']
-departamento= data['DEPARTAMENTO'].unique()
-
-#edad= df['EDAD_DECLARADA'].unique().tolist()
-#edad = np.sort(df['EDAD_DECLARADA'].dropna().unique())
-
-#crear un slider de edad
-edad_selector = st.slider('Edad del fallecido: ',
-                         min_value = min(edad),
-                         max_value = max(edad),
-                         value = (min(edad), max(edad)))
-
-
-#sexo_selector = st.multiselect('SEXO:', sexo, default = ciudad)
-st.write(sexo)
-st.write(departamento)
-opcion_departamento = st.selectbox('Selecciona un departamento:', departamento)
-
-
-
 
 criterio = data['CLASIFICACION_DEF'].unique()
 option_criterio = st.selectbox('Lista de fallecidos según el criterio ' , criterio)
