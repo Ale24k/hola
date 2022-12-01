74615a80dbb50046dad3cbaa1b866540fc4efc78
Ale24k <89529810+Ale24k@users.noreply.github.com>
2022-11-30 21:54:22 -0500

    Update nose.py

--- nose.py
+++ nose.py
@@ -1,40 +1,39 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 import numpy as np
-import gdown
-import os
+import pydeck as pdk
+import plotly.express as px
+
+DATA_URL = (
+    "C:\\Users\\gamboa\\PycharmProjects\\jgamboaData\\dataset\\Motor_Vehicle_Collisions_-_Crashes.csv"
+)
+
+st.markdown("<h1 style='text-align: center; color: black;'>Geographic Data</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>Dataset of Vehicle Collisions</h1>", unsafe_allow_html=True)
+st.markdown('<style>h2{color: blue; text-align:center;}</style>', unsafe_allow_html=True)
 
 
-st.title('Fallecidos por COVID19')
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[["CRASH DATE", "CRASH TIME"]])
+    data.dropna(subset=["LATITUDE", "LONGITUDE"], inplace=True)
+    data.drop(data[data['LATITUDE'] == 0].index, inplace=True)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data.rename(columns={'crash date_crash time': 'date/time'}, inplace=True)
+    data.rename(columns={'number of persons injured': 'injured_persons'}, inplace=True)
+    data.rename(columns={'number of pedestrians injured': 'injured_pedestrians'}, inplace=True)
+    data.rename(columns={'number of cyclist injured': 'injured_cyclists'}, inplace=True)
+    data.rename(columns={'number of motorist injured': 'injured_motorists'}, inplace=True)
+    return data
 
+data = load_data(600000)
 
-# id = 1dSRlbtutz10Lgb4wiYPcWaK3w5QMUH8O
-@st.experimental_memo
-def download_data():
-    #https://drive.google.com/uc?id=YOURFILEID\
-    url = "https://drive.google.com/uc?id=1dSRlbtutz10Lgb4wiYPcWaK3w5QMUH8O"
-    output = 'data.csv'
-    gdown.download(url,output,quiet = False)
+st.sidebar.header("jgamboa")
+st.header("Number of Injured Person x Collision")
 
-download_data()
-#vamos a sacar el primer millon de datos:
-data = pd.read_csv('data.csv', sep = ';', parse_dates= ['FECHA_CORTE', 'FECHA_FALLECIMIENTO'])
-st.dataframe(data.head(20))
-#df = df.drop(columns = ["FECHA_CORTE","FECHA_FALLECIMIENTO","EDAD_DECLARADA","SEXO", "CLASIFICACION_DEF", "DEPARTAMENTO", "PROVINCIA", "DISTRITO", "UBIGEO", "UUID"])
-
-
-
-#SELECCIÓN DE CRITERIOS
-criterio = data['CLASIFICACION_DEF'].unique()
-option_criterio = st.selectbox('Lista de fallecidos según el criterio ' , criterio)
-
-#if option_criterio == '
-#GRAFICO DE BARRAS DE LOS CRITERIOS
-df_criterios = df[df['CLASIFICACION_DEF'] == option_criterio]
-df_crit = df_criterios.CLASIFICACION_DEF.value_counts()
-st.write('Distribución por criterios')
-st.bar_chart(df_crit)
-
-
-#if option_criterio == 'Criterio SINADEF':
-    #B = df[['CLASIFICACION_DEF']].groupby('CLASIFICACION_DEF').count()
+st.sidebar.header("Filter Parameters")
+st.sidebar.header("where are most people injure?")
+#injured_people = st.sidebar.slider("# Person injured in Collisions", 0, 9)
+injured_people = st.sidebar.number_input("Number Person injured in Collisions", step=1, min_value=0, max_value=9, value=1)
+st.map(data.query('injured_persons > @injured_people')[["latitude", "longitude"]].dropna(how="any"))
