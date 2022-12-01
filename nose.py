87270c35a013158c1075dc9980bfcfac76532000
Ale24k <89529810+Ale24k@users.noreply.github.com>
2022-11-30 21:55:11 -0500

    Update nose.py

--- nose.py
+++ nose.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import plotly.express as px
+
 
 DATA_URL = (
     "C:\\Users\\gamboa\\PycharmProjects\\jgamboaData\\dataset\\Motor_Vehicle_Collisions_-_Crashes.csv"
